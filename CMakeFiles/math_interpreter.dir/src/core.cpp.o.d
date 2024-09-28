da8496a6fdbe020c54763996b5e4a90d3e18d3f4
Andrey <gavrin.01@bk.ru>
2024-09-28 21:17:37 +0300

    rework

--- CMakeFiles/math_interpreter.dir/src/core.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/math_interpreter.dir/src/core.cpp.o: \
- /home/massonskyi/sp2024/math_imp/src/core.cpp /usr/include/stdc-predef.h
--- CMakeFiles/math_interpreter.dir/src/io.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/math_interpreter.dir/src/io.cpp.o: \
- /home/massonskyi/sp2024/math_imp/src/io.cpp /usr/include/stdc-predef.h
